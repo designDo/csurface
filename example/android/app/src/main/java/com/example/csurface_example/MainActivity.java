56eb64f2837cbac8ef299d96ca52933fe3731cbc
tangmaking <tangmaking@163.com>
2024-07-11 20:09:04 +0800

    获取系统支持的最高OpenGL版本

--- example/android/app/src/main/java/com/example/csurface_example/MainActivity.java
+++ example/android/app/src/main/java/com/example/csurface_example/MainActivity.java
@@ -1,6 +1,37 @@
 package com.example.csurface_example;
 
+import android.content.pm.ConfigurationInfo;
+import android.content.pm.FeatureInfo;
+import android.content.pm.PackageManager;
+import android.os.Bundle;
+import android.util.Log;
+
+import androidx.annotation.Nullable;
+
 import io.flutter.embedding.android.FlutterActivity;
 
 public class MainActivity extends FlutterActivity {
+
+    private static final String TAG = MainActivity.class.getSimpleName();
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        PackageManager packageManager = getPackageManager();
+        FeatureInfo[] systemAvailableFeatures = packageManager.getSystemAvailableFeatures();
+        FeatureInfo configurationInfo = null;
+        for (FeatureInfo systemAvailableFeature : systemAvailableFeatures) {
+            if (systemAvailableFeature.name == null && systemAvailableFeature.reqGlEsVersion != ConfigurationInfo.GL_ES_VERSION_UNDEFINED) {
+                configurationInfo = systemAvailableFeature;
+            }
+        }
+        if (configurationInfo != null) {
+            int glEsVersion = configurationInfo.reqGlEsVersion;
+            String glEsVersionString = ((glEsVersion & 0xffff0000) >> 16) + "." + (glEsVersion & 0xffff);
+
+            Log.d(TAG, "OpenGL ES Version: " + glEsVersionString);
+        } else {
+            Log.d(TAG, "OpenGL ES Version not found");
+        }
+    }
 }
